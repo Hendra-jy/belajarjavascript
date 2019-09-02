9ff000459623820e7e6dba06fc5b3f0a812b8d8f
Hendra <pmghendra@gmail.com>
2019-09-02 16:04:48 +0700

    [hendra]-membuat index.html dan javascript.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var  i = 1;
+var y = 2; 
+var t = i+y;
+document.write(i);
+document.write(y);
+document.write (t+"<br>");
+
+function testing(){
+    alert("halo selamat kamu sudah berhasil memanggil function di html");
+}
+
+function hendra (a, b){
+    document.write(a+' memakan '+b+"<br>");
+}
+hendra("singa","buaya");
+hendra("akku","kamu");
